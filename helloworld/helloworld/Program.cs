470cb57fe56a97cc89307a84b1179303bc27a765
NannKhaingZar <nannkhaingzathinn@gmail.com>
2018-09-21 22:47:36 +0630

    My first HelloWorld

---
+++ helloworld/helloworld/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace helloworld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Number =");
+            String a = Console.ReadLine();
+            int b = int.Parse(a);
+            Console.WriteLine("Results"+(b+10));
+            Console.ReadLine();
+        }
+    }
+}
