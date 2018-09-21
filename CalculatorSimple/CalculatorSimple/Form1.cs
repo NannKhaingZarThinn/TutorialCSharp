bdb5ef22d4af4f73c3614b788562eb939c966423
NannKhaingZar <nannkhaingzathinn@gmail.com>
2018-09-21 22:50:47 +0630

    mini project for Simple Calculator

---
+++ CalculatorSimple/CalculatorSimple/Form1.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculatorSimple
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        float num, ans;
+        int count;
+
+        public void disable() // Create One Method to disable calculator
+        {
+            textBox1.Enabled = false;
+            button20.Show();
+            button1.Hide();
+            button2.Enabled = false;
+            button3.Enabled = false;
+            button4.Enabled = false;
+            button4.Enabled = false;
+            button5.Enabled = false;
+            button6.Enabled = false;
+            button7.Enabled = false;
+            button8.Enabled = false;
+            button9.Enabled = false;
+            button10.Enabled = false;
+            button11.Enabled = false;
+            button12.Enabled = false;
+            button13.Enabled = false;
+            button14.Enabled = false;
+            button15.Enabled = false;
+            button16.Enabled = false;
+            button17.Enabled = false;
+            button18.Enabled = false;
+            button19.Enabled = false;
+
+        }
+        public void enable()
+        {
+            textBox1.Enabled = true;
+            button1.Show();
+            button20.Hide();
+            button2.Enabled = true;
+            button3.Enabled = true;
+            button4.Enabled = true;
+            button4.Enabled = true;
+            button5.Enabled = true;
+            button6.Enabled = true;
+            button7.Enabled = true;
+            button8.Enabled = true;
+            button9.Enabled = true;
+            button10.Enabled = true;
+            button11.Enabled = true;
+            button12.Enabled = true;
+            button13.Enabled = true;
+            button14.Enabled = true;
+            button15.Enabled = true;
+            button16.Enabled = true;
+            button17.Enabled = true;
+            button18.Enabled = true;
+            button19.Enabled = true;
+        }
+        private void button3_Click(object sender, EventArgs e) 
+        {
+            textBox1.Text = "";
+        }
+
+        private void button12_Click(object sender, EventArgs e)// For Multiplication button
+        {
+            num = float.Parse(textBox1.Text);
+            textBox1.Clear();
+            textBox1.Focus();
+            count = 3;
+            label1.Text = num.ToString() + "*";
+        }
+
+        private void button18_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + ".";
+            textBox1.ForeColor = Color.Red;
+        }
+
+        private void button17_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 0;
+            textBox1.ForeColor = Color.Red;
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 1;
+            textBox1.ForeColor = Color.Red;
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 2;
+            textBox1.ForeColor = Color.Red;
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 3;
+            textBox1.ForeColor = Color.Red;
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 4;
+            textBox1.ForeColor = Color.Red;
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 5;
+            textBox1.ForeColor = Color.Red;
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 6;
+            textBox1.ForeColor = Color.Red;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 7;
+            textBox1.ForeColor = Color.Red;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 8;
+            textBox1.ForeColor = Color.Red;
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 9;
+            textBox1.ForeColor = Color.Red;
+        }
+
+        private void button1_Click(object sender, EventArgs e) // OFF Button
+        {
+            disable(); // Call disable function to Click OFF button
+        }
+
+        private void button20_Click(object sender, EventArgs e)
+        {
+            enable();
+        }
+
+        private void button4_Click(object sender, EventArgs e) // for ADDING button
+        {
+            num = float.Parse(textBox1.Text);
+            textBox1.Clear();
+            textBox1.Focus();
+            count = 1;
+            label1.Text = num.ToString()+ "+";
+        }
+
+        private void button8_Click(object sender, EventArgs e) //for Substraction button
+        {
+            num = float.Parse(textBox1.Text);
+            textBox1.Clear();
+            textBox1.Focus();
+            count = 2;
+            label1.Text = num.ToString() + "-";
+        }
+
+        private void button15_Click(object sender, EventArgs e) // for division button
+        {
+            num = float.Parse(textBox1.Text);
+            textBox1.Clear();
+            textBox1.Focus();
+            count = 4;
+            label1.Text = num.ToString() + "/";
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button19_Click(object sender, EventArgs e)
+        {
+            compute(); //call compute() method function to perform such operation
+            label1.Text = ""; // clear the text on the label
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int length = textBox1.TextLength - 1;
+            string text = textBox1.Text;
+            textBox1.Clear();
+            for (int i=0; i< length; i++)
+            {
+                textBox1.Text = textBox1.Text + text[i];
+            }
+        }
+
+        public void compute()
+        {
+            switch(count)
+            {
+                case 1:
+                    ans = num + float.Parse(textBox1.Text); // It performs addition
+                    textBox1.Text = ans.ToString();
+                    break;
+                case 2:
+                    ans = num - float.Parse(textBox1.Text); // It performs substration
+                    textBox1.Text = ans.ToString();
+                    break;
+                case 3:
+                    ans = num * float.Parse(textBox1.Text); // // It performs multiplication
+                    textBox1.Text = ans.ToString();
+                    break;
+                case 4:
+                    ans = num / float.Parse(textBox1.Text); // It performs Division
+                    textBox1.Text = ans.ToString();
+                    break;
+
+                default:
+                    break;
+            }
+        }
+    }
+}
